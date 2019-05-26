cf83d682d8bbf575ddfcb667cee8292ce51bf3d9
Carlos Arturo <camp.92@hotmail.com>
2019-05-26 15:33:35 -0500

    Add high and low

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -4,7 +4,8 @@ const forecast = (longitud, latitud, callback) => {
     const url = 'https://api.darksky.net/forecast/b11bd9d1037818521971d48b00f3b95a/' + encodeURIComponent(longitud) + ',' + encodeURIComponent(latitud) + '?units=si'
 
     request({url, json:true}, (error, {body}) => {
-        const currently = body.currently        
+        const currently = body.currently
+        const daily = body.daily.data[0]
 
         if(error){
             callback('Unable to connect to weather services!')
@@ -12,8 +13,8 @@ const forecast = (longitud, latitud, callback) => {
             callback('Unable to find location!')
         }
         else{
-            
-            callback('',currently.summary + ', It is currently ' + currently.temperature + ' degrees out. There is a ' + currently.precipProbability + '% chance of rain')  
+            console.log(url)
+            callback('',currently.summary + ', It is currently ' + currently.temperature + ' degrees out the high today is ' + daily.temperatureMax + ' and the low is '+ daily.temperatureMin +'. There is a ' + currently.precipProbability + '% chance of rain')  
         }
     })
 
